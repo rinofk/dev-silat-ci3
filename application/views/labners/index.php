62426e65084bfcd4691dd43e38bca6f90bcfae30
rinofk <rino.f@untan.ac.id>
2025-08-21 13:51:23 +0700

    update label bebas lab profesi ners

--- application/views/labners/index.php
+++ application/views/labners/index.php
@@ -110,7 +110,7 @@
     <!-- Table: Belum Valid -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Berkas Bebas Lab Prodi S1 Keperawatan</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Berkas Bebas Lab Prodi Profesi Ners</h6>
         </div>
         <div class="card-body">
             <div class="table-responsive">
