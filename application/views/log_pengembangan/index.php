c84d5cb995409a7986d61e90f5dafd593bc47f85
rinofk <rino.f@untan.ac.id>
2025-08-21 13:55:08 +0700

    update layout log_pengembangan

--- application/views/log_pengembangan/index.php
+++ application/views/log_pengembangan/index.php
@@ -10,17 +10,44 @@
             <h6 class="m-0 font-weight-bold text-primary">Daftar Log Pengembangan</h6>
         </div>
         <div class="card-body">
-            <ul class="list-group">
-                <?php foreach ($logs as $log): ?>
-                    <li class="list-group-item">
-                        <strong>[<?= ucfirst($log['kategori']); ?>]</strong> 
-                        <span class="text-primary"><?= $log['judul']; ?></span>  
-                        <br>
-                        <small><?= $log['deskripsi']; ?></small><br>
-                        <small class="text-muted"><?= $log['tanggal']; ?> - <?= $log['dibuat_oleh']; ?></small>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTableLog" width="100%" cellspacing="0">
+                    <thead class="thead-light">
+                        <tr>
+                            <th>No</th>
+                            <th>Kategori</th>
+                            <th>Judul</th>
+                            <th>Deskripsi</th>
+                            <th>Tanggal</th>
+                            <th>Dibuat Oleh</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $no=1; foreach ($logs as $log): ?>
+                            <tr>
+                                <td><?= $no++; ?></td>
+                                <td><span class="badge badge-info"><?= ucfirst($log['kategori']); ?></span></td>
+                                <td class="text-primary font-weight-bold"><?= $log['judul']; ?></td>
+                                <td><?= $log['deskripsi']; ?></td>
+                                <td><?= $log['tanggal']; ?></td>
+                                <td><?= $log['dibuat_oleh']; ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
+
+<!-- DataTables Script -->
+<script>
+    $(document).ready(function () {
+        $('#dataTableLog').DataTable({
+            "pageLength": 10,
+            "ordering": true,
+            "lengthChange": true,
+            "autoWidth": false
+        });
+    });
+</script>
